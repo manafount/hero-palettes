f503222eb055f68d0600fe308bde04bb58ab3dd4
Matt Kerchner <manafount@gmail.com>
2017-07-16 18:17:22 -0700

    removed example code from imageDownloader

--- src/imageDownloader.js
+++ src/imageDownloader.js
@@ -8,19 +8,27 @@ let marvel = api.createClient({
   privateKey: process.env.MARVEL_PRIVATE
 });
 
-let example = (callback) => {
-  let bar = new progress(':bar', { total: 10 });
-  let timer = setInterval(function () {
-    bar.tick();
-    if (bar.complete) {
-      clearInterval(timer);
-    }
-  }, 100);
-  // callback(null, 'complete');
+let getUrlBatch = (offset) => {
+  marvel.characters.findAll(100, offset)
+  .then((res) => {
+    let chars = [];
+    console.log(res.meta.total);
+    res.data.forEach(item => {
+      chars.push({name: item.name, image: item.thumbnail});
+    });
+    return chars;
+  })
+  .then((data) => {
+    console.log(data.length);
+  })
+  .fail(console.error)
+  .done();
 };
 
+let getAllCharacterImageUrls = {};
+
 async.waterfall([
-  example
+  getAllCharacterImageUrls
 ], function (err, result) {
     if (err) {
       console.log(err);
@@ -29,19 +37,3 @@ async.waterfall([
     }
 });
 
-
-
-// marvel.characters.findAll(100)
-//   .then((res) => {
-//     let chars = [];
-//     console.log(res.meta.total);
-//     res.data.forEach(item => {
-//       chars.push({name: item.name, image: item.thumbnail});
-//     });
-//     return chars;
-//   })
-//   .then((data) => {
-//     console.log(data.length);
-//   })
-//   .fail(console.error)
-//   .done();
