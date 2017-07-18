c22e48364ab741d3ce8e599fff334cb4e5488e65
Matt Kerchner <manafount@gmail.com>
2017-07-18 10:42:27 -0700

    Palette generation with node-vibrant works. Palettes are saved to a json file for testing purposes. As the file is small and not likely to be rewritten, saving the palettes in a database may not be necessary.

--- src/paletteGenerator.js
+++ src/paletteGenerator.js
@@ -1,8 +1,26 @@
 let Vibrant = require('node-vibrant');
 let fs = require('fs');
+let jsonfile = require('jsonfile');
+let Promise = require('bluebird');
 
-let dir = './images';
-// count number of images in directory (should be 1485)
-fs.readdir(dir, (err, files) => {
-  console.log(files.length);
+let basePath = './images/';
+let output = './data/palettes.json';
+
+fs.readdir(basePath, (err, files) => {
+  let palettes = [];
+  console.log(`Generating ${files.length} palettes...`);
+  Promise.all(files.map((file) => {
+    return Vibrant.from(basePath + files[0]).getPalette()
+      .then((palette) => {
+        palettes.push({id: files[0].slice(0, -3), palette });
+      });
+  }))
+  .then(() => {
+    let obj = {
+      timestamp: Date.now(),
+      data: palettes
+    };
+    jsonfile.writeFile(output, obj);
+    console.log('Complete!');
+  });
 });
