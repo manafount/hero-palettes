0436970f9707a41547459d5484125ce9be414eea
Matt Kerchner <manafount@gmail.com>
2017-08-09 01:56:32 -0700

    src directory cleanup

---
+++ src/Components/Exporter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import autoBind from 'react-autobind';
+
+class Exporter extends Component {
+  constructor(props) {
+    super(props);
+
+    autoBind(this);
+  }
+
+  
+}
