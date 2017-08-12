4762274bd8ae1fa3f9361d06c9058673f15f1fc6
Matt Kerchner <manafount@gmail.com>
2017-08-12 16:35:40 -0700

    font-awesome icons added, directory restructured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
-import './reset.css';
-import './App.css';
-import './Loader.css';
+import '.stylesheets/reset.css';
+import '.stylesheets/app.css';
+import '.stylesheets/font-awesome/css/font-awesome.min.css';
+import '.styleshsets/loader.css';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
 
